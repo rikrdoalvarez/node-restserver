404b637b221694f99d7296d401056530dc83968f
Ricardo <rikrdo.alvarez@gmail.com>
2020-04-29 10:57:22 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:lyLpLGnWPmjxk0Cr@cluster1-mzpii.gcp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
